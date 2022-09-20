10d33cea14bc1e4d7d81576a1653252c251b2a67
LinChiaAn <dan870627@gmail.com>
2022-09-21 02:44:54 +0800

    [Master]建立Model

---
+++ Guestbooks/Models/Guestbook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Guestbooks.Models
+{
+    public class Guestbook
+    {
+        public int id { get; set; }
+        
+        public string Name { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreateTime { get; set; }
+    }
+}
---
+++ Guestbooks/ViewModels/GestbooksViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Guestbooks.Models;
+
+namespace Guestbooks.ViewModels
+{
+    public class GestbooksViewModel
+    {
+        public List<Guestbook> DataList { get; set; }
+    }
+}
